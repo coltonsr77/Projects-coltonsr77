c3a4dbe446470deb2cd7bd91b047d370002949fb
coltonsr77 <coltonr3@icloud.com>
2025-12-28 13:26:02 -0500

    Update Installer0.2-Beta.bat

--- Installer/Installer0.2-Beta.bat
+++ Installer/Installer0.2-Beta.bat
@@ -1,3 +1,3 @@
 @echo off
-winget update && winget install --id=Git.Git -e  && winget install --id=Microsoft.DotNet.Framework.DeveloperPack_4 -e  && winget install --id=Microsoft.DotNet.DesktopRuntime.6 -e  && winget install --id=Microsoft.DirectX -e  && winget install --id=Python.Python.3.14 -e && python -m pip install kitecmd 
+winget update && winget install --id=Git.Git -e  &&  winget install -e --id Microsoft.DotNet.SDK.10 && winget install --id=Microsoft.DotNet.DesktopRuntime.6 -e  && winget install --id=Microsoft.DirectX -e  && winget install --id=Python.Python.3.14 -e && python -m pip install kitecmd 
 @echo off
