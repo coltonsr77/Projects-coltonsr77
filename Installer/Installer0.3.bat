ceb1a6cfa3ceef4ec2b63462fa53967e36d5e36a
coltonsr77 <coltonr3@icloud.com>
2025-12-29 02:30:59 -0500

    Update and rename Installer0.3-Beta.bat to Installer0.3.bat

--- Installer/Installer0.3.bat
+++ Installer/Installer0.3.bat
@@ -1,5 +1,5 @@
 @echo off
-winget update && winget install --id Git.Git -e  &&  winget install --id Microsoft.DotNet.SDK.10 -e
+winget install --id Git.Git -e  &&  winget install --id Microsoft.DotNet.SDK.10 -e
 winget install --id Microsoft.DotNet.DesktopRuntime.6 -e  && winget install --id Microsoft.DirectX -e
 winget install --id Python.Python.3.14 -e && python -m pip install kitecmd && python -m pip install kitecmd-IR==0.1
 @echo off
